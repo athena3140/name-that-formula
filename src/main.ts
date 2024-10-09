76cbbc6e192f20e3d2cfbd3cf89566c7886dd740
athena3140 <athena3140@gmail.com>
2024-10-10 01:36:00 +0630

    add @vercel/analytics

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { inject } from "@vercel/analytics";
 import "./assets/tailwind.css";
 import "./main.css";
 
+inject();
 createApp(App).use(store).use(router).mount("#app");
